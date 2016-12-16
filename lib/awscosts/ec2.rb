8ea3cfd1ff26f014071a789fa38706ef974e9f4a
Chanakya Devraj <chanakya.devraj@cuelogic.co.in>
2016-12-16 18:46:51 +0530

    Update ec2.rb

--- lib/awscosts/ec2.rb
+++ lib/awscosts/ec2.rb
@@ -26,7 +26,7 @@ class AWSCosts::EC2
     'ap-south-1' => "ap-south-1",
     'eu-west-2' => "eu-west-2",
     'us-east-2' => "us-east-2",
-    'ca-central-1' => "ca-central-1",
+    'ca-central-1' => "ca-central-1"
   }
 
   EBS_RAW_MAPPING = %w(us-east-1 us-west-1 eu-west-1 ap-southeast-1 ap-southeast-2 ap-northeast-1)
