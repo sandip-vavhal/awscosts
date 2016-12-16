5020688f4acc6ea615a5176d3fda460dec2e658e
Chanakya Devraj <chanakya.devraj@cuelogic.co.in>
2016-12-16 13:44:56 +0530

    Update rds.rb

--- lib/awscosts/rds.rb
+++ lib/awscosts/rds.rb
@@ -12,7 +12,7 @@ class AWSCosts::Rds
        'us-west-1' => "us-west-1",
        'us-west-2' => "us-west-2",
        'eu-west-1' => "eu-west-1",
-        'eu-central-1' => "eu-central-1",
+       'eu-central-1' => "eu-central-1",
        'ap-southeast-1' => "ap-southeast-1",
        'ap-southeast-2' =>"ap-southeast-2",
        'ap-northeast-1' =>"ap-northeast-1",
