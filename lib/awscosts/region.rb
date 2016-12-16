46e5a505b6273b3ebf9fd3cfd00d36b72d8e0924
Chanakya Devraj <chanakya.devraj@cuelogic.co.in>
2016-12-16 19:00:39 +0530

    Update region.rb

--- lib/awscosts/region.rb
+++ lib/awscosts/region.rb
@@ -14,7 +14,10 @@ class AWSCosts::Region
     'ap-northeast-1' => { :full_name => 'Asia Pacific (Tokyo)', :price_mapping => 'ap-northeast-1', :emr_mapping => 'apac-tokyo' },
     'sa-east-1' => { :full_name => 'South America (Sao Paulo)', :price_mapping => 'sa-east-1', :emr_mapping => 'sa-east-1' },
     'ap-northeast-2' => { :full_name => 'Asia Pacific (Seoul)', :price_mapping => 'ap-northeast-2', :emr_mapping => 'ap-northeast-2' },
-    'ap-south-1' => { :full_name => 'Asia Pacific (Mumbai)', :price_mapping => 'ap-south-1', :emr_mapping => 'ap-south-1' }
+    'ap-south-1' => { :full_name => 'Asia Pacific (Mumbai)', :price_mapping => 'ap-south-1', :emr_mapping => 'ap-south-1' },
+    'us-east-2' => { :full_name => 'US East (Ohio)', :price_mapping => 'us-east-2', :emr_mapping => 'us-east-2' },
+    'ca-central-1' => { :full_name => 'Canada (Central)', :price_mapping => 'ca-central-1', :emr_mapping => 'ca-central-1' },
+    'eu-west-2' => { :full_name => 'EU (London)', :price_mapping => 'eu-west-2', :emr_mapping => 'eu-west-2' }
   }
 
   def self.find name
